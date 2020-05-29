6be399a8cef47c32d6f0577901d8b40d6ed833cf
aloyonnet <off.alexis.loyonnet@gmail.com>
2020-05-29 19:43:32 +0200

    feat: fake users on DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,47 @@
 <?php
 
-use Illuminate\Database\Seeder;
+    use Illuminate\Database\Seeder;
+    use Illuminate\Support\Facades\DB;
+    use Illuminate\Support\Facades\Hash;
+    use Illuminate\Support\Str;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    class DatabaseSeeder extends Seeder
     {
-        // $this->call(UserSeeder::class);
+        /**
+         * Run the database seeds.
+         *
+         * @return void
+         */
+        public function run()
+        {
+            DB::table('users')->insert([
+                'name' => 'admin-user',
+                'email' => 'admin-user@gmail.com',
+                'password' => Hash::make('password'),
+            ]);
+
+            DB::table('users')->insert([
+                'name' => 'normal-user',
+                'email' => 'normal-user@gmail.com',
+                'password' => Hash::make('password'),
+            ]);
+
+            DB::table('roles')->insert([
+                'name' => 'admin',
+            ]);
+
+            DB::table('roles')->insert([
+                'name' => 'user',
+            ]);
+
+            DB::table('role_user')->insert([
+                'role_id' => 1,
+                'user_id' => 1,
+            ]);
+
+            DB::table('role_user')->insert([
+                'role_id' => 2,
+                'user_id' => 2,
+            ]);
+        }
     }
-}
