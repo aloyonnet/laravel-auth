7b1f90a9728e9901b64f1be4fe98c53cb690c0f6
aloyonnet <off.alexis.loyonnet@gmail.com>
2020-05-29 19:15:04 +0200

    feat: admin user crud with roles

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -3,31 +3,42 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Role;
 use App\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class UsersController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
-     *
-     * @return Response
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::all();
+        return view('admin.users.index', [
+            'users' => $users
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param User $user
-     * @return Response
      */
     public function edit(User $user)
     {
-        //
+        $roles = Role::all();
+
+        return view('admin.users.edit', [
+            'user' => $user,
+            'roles' => $roles
+        ]);
     }
 
     /**
@@ -35,21 +46,25 @@ class UsersController extends Controller
      *
      * @param Request $request
      * @param User $user
-     * @return Response
      */
     public function update(Request $request, User $user)
     {
-        //
+        $user->roles()->sync($request->roles);
+
+        return redirect()->route('admin.users.index');
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param User $user
-     * @return Response
+     * @throws Exception
      */
     public function destroy(User $user)
     {
-        //
+        $user->roles()->detach();
+        $user->delete();
+
+        return redirect()->route('admin.users.index');
     }
 }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,18 +3,42 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Users</div>
 
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        nothing for the moment
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Roles</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($users as $u)
+                                    <tr>
+                                        <th scope="row">{{ $u->id }}</th>
+                                        <td>{{ $u->name }}</td>
+                                        <td>{{implode(', ', $u->roles()->get()->pluck('name')->toArray()) }}</td>
+                                        <td>{{ $u->email }}</td>
+                                        <td>
+                                            <a href="{{ route('admin.users.edit', $u->id) }}"><button type="button" class="btn btn-success float-left">Edit</button></a>
+                                            <form action="{{ route('admin.users.destroy', $u) }}" method="POST" class="float-left">
+                                                @csrf
+                                                {{ method_field('DELETE') }}
+                                                <button type="submit" class="btn btn-danger">
+                                                    Delete
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
